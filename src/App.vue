c5d5750b26e664af43289b29ef518381f587354a
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2023-08-21 11:36:09 -0500

    feat: reactive data binding

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Reactive Data Binding</router-link> |
+    <router-link to="/about">Component Communication</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/ReactiveDataBinding.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = ref(0)
+const incrementCounterRef = () => {
+  counter.value++;
+}
+
+const state = ref({counter: 0})
+const incrementCounterReactive = () => {
+  state.value.counter++;
+}
+
+</script>
+
+<template>
+  <h1>Reactive Data Binding</h1>
+  <div class="flex flex-col">
+    <div>
+      <p>Counter Ref is: {{ counter }}</p>
+      <button @click="incrementCounterRef">Increment by 1 Ref</button>
+    </div>
+    <div>
+      vs
+    </div>
+    <div>
+      <p>Counter Reactive is: {{ state.counter }}</p>
+      <button @click="incrementCounterReactive">Increment by 1 Reactive</button>
+    </div>
+  </div>
+</template>
