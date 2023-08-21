d0c0ad76d1bd258c94ed21dcb36eb14108aa7f12
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2023-08-21 13:28:17 -0500

    feat: component communication styles

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -3,5 +3,5 @@ defineProps(['message'])
 </script>
 
 <template>
-  <p>The message is: {{ message }}</p>
+  <p>The message is: <span class="text-red-500 font-bold">{{ message }}</span></p>
 </template>
--- src/views/ComponentCommunication.vue
+++ src/views/ComponentCommunication.vue
@@ -1,25 +1,26 @@
 <script setup>
-import { ref } from 'vue'
-import ChildComponent from '@/components/ChildComponent.vue';
+  import { ref } from 'vue'
+  import ChildComponent from '@/components/ChildComponent.vue';
 
-const message = ref('')
-const generateNewMessage = () => {
-  message.value = (Math.random() + 1).toString(36).substring(7);
-}
-
+  const message = ref('')
+  const generateNewMessage = () => {
+    message.value = (Math.random() + 1).toString(36).substring(7);
+  }
 </script>
 
 <template>
-  <!--
-  - Component Communication: 
-  Create two Vue 3 components - a parent and a child component. 
-  The parent component should have a button; when the button is clicked, it should send a message to the child component. 
-  The child component should display the received message.-->
-  <h1>component communication</h1>
-  <div class="flex flex-col">
-    <div>
-      <ChildComponent :message="message"/>
-      <button @click="generateNewMessage">Send new message</button>
+  <div class="bg-white p-4 rounded-md max-w-3xl mx-auto w-11/12">
+    <h1 class="font-bold text-left mb-4">Component communication</h1>
+    <div class="flex flex-col">
+      <div>
+        <ChildComponent :message="message"/>
+        <button
+          class="bg-transparent mt-4 hover:bg-blue-500 w-full text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-1"
+          @click="generateNewMessage"
+        >
+          Send new message
+        </button>
+      </div>
     </div>
   </div>
 </template>
